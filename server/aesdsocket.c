ca8b20b6939762736048dee21d31039bddb26bf5
GavinC <gavin.chuang@wnc.com.tw>
2024-12-25 11:13:06 +0800

    assignment5-1: Add syslog and '\0' at the end of string

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -4,9 +4,51 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <syslog.h>
+#include <signal.h>
 
 #define PORT 9000
 #define BUFFER_SIZE 1024
+
+const char *filename = "/var/tmp/aesdsocketdata";
+static int serverFD, clientFD;
+static FILE *file;
+
+void handleSIGINT(int sig){
+    syslog(LOG_DEBUG, "Caught signal, exiting");
+
+    // Closing any open sockets, and deleting the file /var/tmp/aesdsocketdata.
+    close(serverFD);
+    close(clientFD);
+    
+    if(remove(filename) == 0){
+        printf("File %s deleted successfully.\n", filename);
+    }else
+        perror("Error deleting the file");
+
+    // Close syslog
+    closelog();
+
+    exit(0);
+}
+
+void handleSIGTERM(int sig){
+    // printf("Caught signal, exiting");
+    syslog(LOG_DEBUG, "Caught signal, exiting");
+
+    // Closing any open sockets, and deleting the file /var/tmp/aesdsocketdata.
+    close(serverFD);
+    close(clientFD);
+    
+    if(remove(filename) == 0){
+        printf("File %s deleted successfully.\n", filename);
+    }else
+        perror("Error deleting the file");
+
+    // Close syslog
+    closelog();
+
+    exit(0);
+}
 
 char *readUntilNewLind(int clientFD){
     char buffer[BUFFER_SIZE] = {};
@@ -14,13 +56,14 @@ char *readUntilNewLind(int clientFD){
     size_t totalSize = 0;
 
     while(1){
+        memset(buffer, 0, sizeof(buffer));
         size_t bytesNum = recv(clientFD, buffer, BUFFER_SIZE, 0);
         if (bytesNum < 0){
             perror("Read error");
             free(result);
             return NULL;
         }else if(bytesNum == 0){
-            // EOF
+            // Client was close
             break;
         }
 
@@ -29,22 +72,31 @@ char *readUntilNewLind(int clientFD){
         // Copy to buffer(result)
         memcpy(result + totalSize, buffer, bytesNum);
         totalSize += bytesNum;
-
-        if(buffer[bytesNum-1] == '\n')
+        if(buffer[bytesNum-1] == '\n'){
+            // EOF
+            result[totalSize] = '\0';
             break;
+        }
     };
-
     return result;
 }
 
 int main(){
-    int serverFD, clientFD, one = 1;
     struct sockaddr_in address;
     socklen_t addr_len = sizeof(address);
     char clientIP[INET_ADDRSTRLEN];
-    const char *filename = "/var/tmp/aesdsocketdata";
-    FILE *file;
     char buffer[BUFFER_SIZE] = {};
+    int one = 1;
+
+    if (signal(SIGINT, handleSIGINT) == SIG_ERR) {
+        perror("Unable to set SIGINT handler");
+        exit(1);
+    }
+
+    if (signal(SIGTERM, handleSIGTERM) == SIG_ERR) {
+        perror("Unable to set SIGTERM handler");
+        exit(1);
+    }
 
     if((serverFD = socket(AF_INET, SOCK_STREAM, 0)) == 0){
         perror("socket failed");
@@ -86,7 +138,7 @@ int main(){
 
         // Show the IP of client
         inet_ntop(AF_INET, &address.sin_addr, clientIP, sizeof(clientIP));
-        printf("Accepted connection from %s\n", clientIP);
+        syslog(LOG_DEBUG, "Accepted connection from %s\n", clientIP);
 
         // Open/Create file
         file = fopen(filename, "a+");
@@ -111,8 +163,6 @@ int main(){
         if(file == NULL){
             printf("Cant open file\n");
             return -1;
-        }else{
-            printf("Open file\n");
         }
 
         while(fgets(buffer, BUFFER_SIZE, file) != NULL){
@@ -124,8 +174,7 @@ int main(){
         // Close sockets
         close(clientFD);
         fclose(file);
-        printf("Closed connection from %s\n", clientIP);
-        printf("----------------------------\n");
+        syslog(LOG_DEBUG, "Closed connection from %s\n", clientIP);
     }
     close(serverFD);
     // Close syslog
